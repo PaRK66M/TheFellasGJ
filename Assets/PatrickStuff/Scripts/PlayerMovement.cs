aa0136c944d8bfe6198793fb0b77da607e50a5e9
PaRK66M <patrick.kopecny@gmail.com>
2024-08-17 00:41:18 +0100

    Player Horizontal Movement Update + Flame Trail effect
    Player movement updated to using AddForce
    When the player moves the tile underneath them will be changed to a flame trail tile (needs to be updated to only change existing tiles, check for grounded?)
    
    Potential Bug: Project settings QualitySettings and ShaderGraphSettings were changed for some reasons

--- Assets/PatrickStuff/Scripts/PlayerMovement.cs
+++ Assets/PatrickStuff/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,10 +12,26 @@ public class PlayerMovement : MonoBehaviour
     //Components
     private Rigidbody2D rb;
 
+    //Player
+    public float minimumSize;
+
+    //SpriteMap
+    [SerializeField] private Tilemap groundMap;
+    [SerializeField] private Tile flameTile;
+    [SerializeField] private Transform frontStep;
+
     //Movement
     [SerializeField]
     private float speed;
-    private float horizontalMovmentInput;
+    private float horizontalMovementInput;
+    [SerializeField] 
+    private float movementScaleDecrease;
+
+    private float accelerationAmount;
+    private float decelerationAmount;
+
+    //Debug
+    private Vector3 gizmoBox = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -23,21 +40,49 @@ public class PlayerMovement : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
 
-        horizontalMovmentInput = 0.0f;
+        horizontalMovementInput = 0.0f;
+
+        accelerationAmount = (50 * speed) / speed;
+        decelerationAmount = (50 * speed) / speed;
+
+        EnableInput();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        Vector2 displacement = rb.position;
-        displacement.x += horizontalMovmentInput + speed * Time.fixedDeltaTime;
 
-        rb.MovePosition(displacement);
+        float targetSpeed = horizontalMovementInput * speed;
+
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? accelerationAmount : decelerationAmount;
+
+        float speedDifference = targetSpeed - rb.velocity.x;
+
+        float movement = speedDifference * accelRate;
+
+        rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
+
+        if (horizontalMovementInput > 0.0f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+        else if (horizontalMovementInput < 0.0f)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (movement != 0.0f)
+        {
+            Vector3Int temp = new Vector3Int(Mathf.FloorToInt(frontStep.position.x), Mathf.FloorToInt(frontStep.position.y - 0.1f), 0);
+            gizmoBox = temp;
+            groundMap.SetTile(temp, flameTile);
+        }
+
     }
 
     #region Input
@@ -56,12 +101,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnHorizontalMovementPerformed(InputAction.CallbackContext direction)
     {
-        horizontalMovmentInput = direction.ReadValue<float>();
+        horizontalMovementInput = direction.ReadValue<float>();
     }
 
     private void OnHorizontalMovementCancelled(InputAction.CallbackContext direction) 
     {
-        horizontalMovmentInput = 0.0f;
+        horizontalMovementInput = 0.0f;
     }
+    #endregion
+
+    #region Gizmos
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawCube(new Vector3(gizmoBox.x + 0.5f, gizmoBox.y + 0.5f, 0.0f), Vector3.one);
+    }
+
     #endregion
 }
