478c705cc4e16be7f93692578330d3a607ef1577
Ayame <iris.marian12@gmail.com>
2024-08-17 19:06:36 +0100

    instant fire added working on victory
    bool can be checked on the flammable script to instantly catch fire regardless of size
    victory screen in progress

--- Assets/Iris' lil things/PauseMenu.cs
+++ Assets/Iris' lil things/PauseMenu.cs
@@ -10,6 +10,10 @@ public class PauseMenu : MonoBehaviour
     private GameObject PauseMenuUI;
     [SerializeField]
     private GameObject OptionMenuUI;
+    [SerializeField]
+    private GameObject VictoryScreenUI;
+    [SerializeField]
+    private GameObject TrackerObj;
 
     // Update is called once per frame
     void Update()
@@ -24,6 +28,10 @@ public class PauseMenu : MonoBehaviour
             {
                 Pause();
             }
+        }
+        if (TrackerObj.GetComponent<TrackerScript>().totalFlammableObj - TrackerObj.GetComponent<TrackerScript>().onFireObj <= 0)
+        {
+            Victory();
         }
     }
 
@@ -52,6 +60,11 @@ public class PauseMenu : MonoBehaviour
     {
         PauseMenuUI.SetActive(true);
         OptionMenuUI.SetActive(false);
+    }
+
+    void Victory()
+    {
+        VictoryScreenUI.SetActive(true);
     }
     //load menu
     //quit game
