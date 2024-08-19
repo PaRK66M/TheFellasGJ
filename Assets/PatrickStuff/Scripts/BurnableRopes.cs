dcc7a30cc9748b8333a012494d51eaeca1cc50ee
PaRK66M <patrick.kopecny@gmail.com>
2024-08-19 18:42:03 +0100

    Rope functionality
    Ropes can now burn and will drop what they are holding onto

--- Assets/PatrickStuff/Scripts/BurnableRopes.cs
+++ Assets/PatrickStuff/Scripts/BurnableRopes.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class BurnableRopes : MonoBehaviour
 {
     private bool onFire;
+    [SerializeField]
+    private bool hasJoint;
+    [SerializeField]
+    private float fuelIncrease;
+
+    private HingeJoint2D joint;
 
     // Start is called before the first frame update
     void Start()
     {
         onFire = false;
+        if (hasJoint)
+        {
+            joint = GetComponent<HingeJoint2D>();
+        }
     }
 
     // Update is called once per frame
@@ -22,7 +32,28 @@ public class BurnableRopes : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
-
+            if (!onFire)
+            {
+                onFire = true;
+                gameObject.layer = 8;
+                SetFireImage();
+                collision.gameObject.GetComponent<PlayerMovement>().EnlargeSize(fuelIncrease);
+                if (hasJoint)
+                {
+                    joint.enabled = false;
+                }
+            }
+            
         }
+    }
+
+    private void SetFireImage()
+    {
+
+    }
+
+    public void RemoveFireImage()
+    {
+
     }
 }
